94124eb25eb7a4646ff9ff996840e89dedb280a3
Ivan Lazarevic <ivanlazarevicopen@outlook.com>
2023-08-01 21:53:57 +0200

    Extract constants

---
+++ employees/src/constants.rs
@@ -0,0 +1,14 @@
+/// Commands
+
+pub const HELP: &str = "help";
+pub const ADD: &str = "add";
+pub const DEPT: &str = "dept";
+pub const DEPT_SORT: &str = "deptsort";
+pub const DEPTS: &str = "depts";
+pub const ALL: &str = "all";
+pub const ALL_BY_DEPT: &str = "allbydept";
+pub const ALL_UNIQUE_PER_DEPT: &str = "unique";
+pub const REMOVE: &str = "remove";
+pub const REMOVE_DEPT: &str = "removedept";
+pub const CLEAR: &str = "clear";
+pub const STOP: &str = "stop";
