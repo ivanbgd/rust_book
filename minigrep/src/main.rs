c8c8d33ac1380989ef9b9ebac9dbc912171f93d5
Ivan Lazarevic <ivanlazarevicopen@outlook.com>
2023-08-06 14:51:45 +0200

    Create main.rs

---
+++ minigrep/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+
+fn main() {
+    let args = env::args().collect::<Vec<String>>();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(-1);
+    });
+
+    // println!("Searching for '{}' in file '{}'.", config.query, config.file_path);  // debug output
+
+    if let Err(err) = minigrep::run(config) {
+        eprintln!("Application error: {}", err);
+        process::exit(-2);
+    }
+}
